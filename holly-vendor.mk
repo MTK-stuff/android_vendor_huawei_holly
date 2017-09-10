1f3e0b9df054a7b99c7a656e26d412494f28b419
kishpatel1998 <kishannn.p.19@gmail.com>
2017-09-10 11:32:31 +0000

    holly : rename

---
+++ holly-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/vendor,system/vendor)
